1f36cd0367dc8e95782dc82f252939e606ab0634
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-14 18:15:07 +0300

    reinit webpack (some prob are resolve

--- app/assets/script/app.js
+++ app/assets/script/app.js
@@ -1,4 +1,12 @@
-var Person = require('./modules/Person')
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.color = favColor;
+  this.greet = function() {
+    console.log("Hello, my name is " + this.name + "and my favorite color is " + favColor + ".");
+  }
+}
+
+
 
 var john = new Person("John Doe", "blue");
 john.greet();
