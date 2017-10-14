3d09274188d34cf1695f1cd8cc237b94b7fc027f
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-14 22:50:39 +0300

    config babel

--- app/assets/script/app.js
+++ app/assets/script/app.js
@@ -1,12 +1,18 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
 
-alert('LAALLALALAL');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in texes. ");
+  }
+}
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Doe", "green");
+var jane = new Person("Jane Doe", "red");
 jane.greet();
+//jane.payTaxes();
 
 $("h1").remove();
--- app/assets/script/modules/Person.js
+++ app/assets/script/modules/Person.js
@@ -1,8 +1,13 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.color = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + this.name + "and my favorite color is " + favColor + ".");
+class Person {
+  constructor(fullName, favColor) {
+    this.name = fullName;
+    this.favoriteColor = favColor;
+  }
+
+  greet() {
+    console.log("yahah!!!,aa my name is " + this.name + "and my favorite color is " + this.favoriteColor + ".");
   }
 }
-module.exports = Person;
+
+// module.exports = Person;
+export default Person;
