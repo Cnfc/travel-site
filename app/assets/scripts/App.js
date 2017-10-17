58cbabd763f0f31f87989345427f8a97af9f2b1a
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-17 21:32:45 +0300

    add revealing scroll elements

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import revealOnScroll from './modules/revealOnScroll';
+
 
 var mobileMenu = new MobileMenu();
-
+var RevealOnScroll = new revealOnScroll();
