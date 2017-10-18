263e29923b9db39135fa02e868c9de31b019bdb2
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-18 22:27:04 +0300

    add N27 button esc

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import MobileMenu from './modules/MobileMenu';
 import revealOnScroll from './modules/revealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
 var mobileMenu = new MobileMenu();
 new revealOnScroll($(".feature-item"), "85%");
 new revealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
